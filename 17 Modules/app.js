0536f62e20cf0cc9e0975b4b957358c809080670
Syed Ahsan Manzar <144564781+syedahsanmanzar@users.noreply.github.com>
2023-11-06 20:08:39 +0500

    Add files via upload

---
+++ 17 Modules/app.js
@@ -0,0 +1,9 @@
+// import message from "./library.js";
+// console.log(message);
+
+// import { obj1, obj2 } from "./library.js"
+// console.log(obj1.fname, obj2.lname);
+
+// import stu1 from "./library.js"
+// console.log(stu1);
+// console.log(stu1.name);
---
+++ 17 Modules/library.js
@@ -0,0 +1,34 @@
+// example 01
+// const message = () => {
+//     const name = "Jesse";
+//     const age = 40;
+//     return name + ' is ' + age + ' years old.';
+// };
+
+// export default message();
+
+
+// example 02
+// const obj1 = {
+//     fname: "syed ahsan"
+// }
+
+// const obj2 = {
+//     lname: "manzar"
+// }
+
+// export { obj1, obj2 };
+
+// example 03
+
+// class Students {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// let stu1 = new Students('ahsan', 20)
+
+
+// export default stu1;
